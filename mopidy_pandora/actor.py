1e03481439141b1813102ff7048c0bfbe95abbff
jcass <john.cass77@gmail.com>
2015-06-23 16:12:10 +0200

    Incorporate decorator provided by @rectalogic.

--- mopidy_pandora/actor.py
+++ mopidy_pandora/actor.py
@@ -4,7 +4,6 @@ import logging
 import urllib
 import pykka
 from mopidy import backend, models
-import pandora
 from mopidy_pandora.pydora import AlwaysOnAPIClient
 
 logger = logging.getLogger(__name__)
@@ -15,20 +14,7 @@ class PandoraBackend(pykka.ThreadingActor, backend.Backend):
 
     def __init__(self, config, audio):
         super(PandoraBackend, self).__init__()
-        config = config['pandora']
-        settings = {
-            "API_HOST": config.get("api_host", 'tuner.pandora.com/services/json/'),
-            "DECRYPTION_KEY": config["partner_decryption_key"],
-            "ENCRYPTION_KEY": config["partner_encryption_key"],
-            "USERNAME": config["partner_username"],
-            "PASSWORD": config["partner_password"],
-            "DEVICE": config["partner_device"],
-            "DEFAULT_AUDIO_QUALITY": config.get("preferred_audio_quality", 'mediumQuality'),
-            "SORT_ORDER": config["sort_order"]
-        }
-        self.api = AlwaysOnAPIClient.from_settings_dict(settings)
-        self.api.login(username=config["username"], password=config["password"])
-
+        self.api = AlwaysOnAPIClient(config['pandora'])
         self.library = PandoraLibraryProvider(backend=self, sort_order=config["sort_order"])
         self.playback = PandoraPlaybackProvider(audio=audio, backend=self)
 
@@ -50,16 +36,11 @@ class PandoraPlaybackProvider(backend.PlaybackProvider):
                 return track
             else:
                 # Tracks have expired, retrieve fresh playlist from Pandora
-                self.tracks = self._get_playlist(station_token)
+                self.tracks = self.backend.api.get_playlist(station_token)
                 return next(self.tracks)
         except StopIteration:
-            # Played through current playlist, retrieve fresh playlist from Pandora
-            self.tracks = self._get_playlist(station_token)
+            self.tracks = self.backend.api.get_playlist(station_token)
             return next(self.tracks)
-
-    def _get_playlist(self, station_token):
-        return (pandora.models.pandora.PlaylistItem.from_json(self.backend.api, station)
-            for station in self.backend.api.get_playlist(station_token)['items'])
 
     def change_track(self, track):
         track_uri = PandoraUri.parse(track.uri)
--- mopidy_pandora/pydora.py
+++ mopidy_pandora/pydora.py
@@ -1,8 +1,23 @@
-from pandora.models.pandora import Station
+import functools
 import requests
-from pandora import BaseAPIClient, APIClient, PandoraException
+import pandora
 
-class AlwaysOnAPIClient(APIClient):
+
+def authenticated(f):
+    @functools.wraps(f)
+    def with_authentication(self, *args, **kwargs):
+        try:
+            return f(self, *args, **kwargs)
+        except pandora.PandoraException as e:
+            if e.message == "Invalid Auth Token":
+                # Re-authenticate and retry if we lost auth
+                self.authenticate()
+                return f(self, *args, **kwargs)
+            raise
+    return with_authentication
+
+
+class AlwaysOnAPIClient(object):
     """Pydora API Client for Mopidy-Pandora
 
     This API client automatically re-authenticates itself if the Pandora authorization token
@@ -12,90 +27,35 @@ class AlwaysOnAPIClient(APIClient):
     Detects 'Invalid Auth Token' messages from the Pandora server, and repeats the last
     request after logging in to the Pandora server again.
     """
+    def __init__(self, config):
+        self.settings = {
+            "API_HOST": config.get("api_host", 'tuner.pandora.com/services/json/'),
+            "DECRYPTION_KEY": config["partner_decryption_key"],
+            "ENCRYPTION_KEY": config["partner_encryption_key"],
+            "USERNAME": config["partner_username"],
+            "PASSWORD": config["partner_password"],
+            "DEVICE": config["partner_device"],
+            "DEFAULT_AUDIO_QUALITY": config.get("preferred_audio_quality", 'mediumQuality')
+        }
+        self.username = config["username"]
+        self.password = config["password"]
+        self.authenticate()
 
-    def __init__(self, transport, partner_user, partner_password, device, default_audio_quality=BaseAPIClient.MED_AUDIO_QUALITY):
-        super(AlwaysOnAPIClient,self).__init__(transport, partner_user, partner_password, device, default_audio_quality)
-        self._stations = []
-        self._station_list_checksum = ""
-
-    def login(self, username, password):
-
-        # Store username and password so that client can re-authenticate itself if required.
-        self.username = username
-        self.password = password
-
-        return super(AlwaysOnAPIClient, self).login(username, password)
-
-
-    def re_authenticate(self):
-
-        # Invalidate old tokens to ensure that the pydora transport layer creates new ones
-        self.transport.user_auth_token = None
-        self.transport.partner_auth_token = None
-
-        # Reset sync times for new Pandora session
-        self.transport.start_time = None
-        self.transport.server_sync_time = None
-
-        self.login(self.username, self.password)
-
+    def authenticate(self):
+        self.api = pandora.APIClient.from_settings_dict(self.settings)
+        self.api.login(username=self.username, password=self.password)
 
     def playable(self, track):
-
         # Retrieve header information of the track's audio_url. Status code 200 implies that
         # the URL is valid and that the track is accessible
-        url = track.audio_url
-        r = requests.head(url)
-        if r.status_code == 200:
-            return True
+        r = requests.head(track.audio_url)
+        return r.status_code == 200
 
-        return False
-
-    def _get_station_list(self):
-        if not self._stations:
-            result = self.transport("user.getStationList", includeStationArtUrl=True)
-            self._station_list_checksum = result['checksum']
-            self._stations = [Station.from_json(self, s)
-                for s in result['stations']]
-            return self._stations
-
-        checksum = self.get_station_list_checksum()['checksum']
-
-        if self._station_list_checksum != checksum:
-            # Station list has been changed by another Pandora client, invalidate and fetch new list.
-            self._stations = []
-            self._get_station_list()
-
-        return self._stations
-
-
+    @authenticated
     def get_station_list(self):
+        return self.api.get_station_list()
 
-        try:
-            return self._get_station_list()
-        except PandoraException as e:
-
-            if e.message == "Invalid Auth Token":
-                self.re_authenticate()
-                return self._get_station_list()
-            else:
-                # Exception is not token related, re-throw to be handled elsewhere
-                raise e
-
-
+    @authenticated
     def get_playlist(self, station_token):
-
-        try:
-            return super(AlwaysOnAPIClient, self).get_playlist(station_token)
-        except PandoraException as e:
-
-            if e.message == "Invalid Auth Token":
-                self.re_authenticate()
-                return super(AlwaysOnAPIClient, self).get_playlist(station_token)
-            else:
-                # Exception is not token related, re-throw to be handled elsewhere
-                raise e
-
-    def get_station_list_checksum(self):
-
-        return self.transport("user.getStationListChecksum")
+        return (pandora.models.pandora.PlaylistItem.from_json(self.api, station)
+                for station in self.api.get_playlist(station_token)['items'])
