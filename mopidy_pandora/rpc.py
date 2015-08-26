4214ea18f0bbf95348dd7add7057efc6acc44f0b
Andrew Wason <rectalogic@rectalogic.com>
2015-08-26 17:03:49 -0400

    Set auto-repeat as soon as the first track in a new station starts to play.

--- mopidy_pandora/rpc.py
+++ mopidy_pandora/rpc.py
@@ -18,11 +18,27 @@ class RPCClient(object):
 
         return requests.request('POST', self.url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
 
-    def set_repeat(self):
+    def set_repeat(self, value=True):
 
-        self._do_rpc('core.tracklist.set_repeat', {'value': True})
+        self._do_rpc('core.tracklist.set_repeat', {'value': value})
+
+    def set_consume(self, value=True):
+
+        self._do_rpc('core.tracklist.set_consume', {'value': value})
+
+    def set_single(self, value=True):
+
+        self._do_rpc('core.tracklist.set_single', {'value': value})
+
+    def set_random(self, value=True):
+
+        self._do_rpc('core.tracklist.set_random', {'value': value})
 
     def get_current_track_uri(self):
 
         response = self._do_rpc('core.playback.get_current_tl_track')
         return response.json()['result']['track']['uri']
+
+    def resume_playback(self):
+
+        self._do_rpc('core.playback.resume')
