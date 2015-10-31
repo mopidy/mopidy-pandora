50c0158136553775d7f5524160aa240b532a8f3d
jcass <john.cass77@gmail.com>
2015-10-31 17:55:50 +0200

    Add reminder to skip retrieving new station list for ad tracks.

--- mopidy_pandora/playback.py
+++ mopidy_pandora/playback.py
@@ -53,7 +53,9 @@ class PandoraPlaybackProvider(backend.PlaybackProvider):
 
         station_id = PandoraUri.parse(track.uri).station_id
 
-        if self._station is None or (station_id is not None and station_id != self._station.id):
+        # TODO: should be able to perform check on is_ad() once dynamic tracklist support is available
+        # if not self._station or (not track.is_ad() and station_id != self._station.id):
+        if self._station is None or (station_id != '' and station_id != self._station.id):
             self._station = self.backend.api.get_station(station_id)
             self._station_iter = iterate_forever(self._station.get_playlist)
 
