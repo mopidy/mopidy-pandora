9846e2b5cedf2fbecc8d7ccf938e307706f85cb7
jcass <john.cass77@gmail.com>
2015-12-27 09:16:26 +0200

    Refactor code for looking up tracks to avoid copying objects on every lookup.
    Add test cases for looking up advertisement tracks.

--- mopidy_pandora/library.py
+++ mopidy_pandora/library.py
@@ -61,36 +61,33 @@ class PandoraLibraryProvider(backend.LibraryProvider):
                 logger.error("Failed to lookup '{}'".format(uri))
                 return []
             else:
+                track_kwargs = {'uri': uri}
+                (album_kwargs, artist_kwargs) = {}, {}
+
+                # TODO: Album.images has been deprecated in Mopidy 1.2. Remove this code when all frontends have been
+                #       updated to make use of the newer LibraryController.get_images()
+                images = self.get_images([uri])[uri]
+                if len(images) > 0:
+                    album_kwargs = {'images': [image.uri for image in images]}
+
                 if type(pandora_uri) is AdItemUri:
-                    if not pandora_track.company_name or len(pandora_track.company_name) == 0:
-                        pandora_track.company_name = 'Unknown'
-
-                    track = models.Track(name='Advertisement',
-                                         uri=uri,
-                                         artists=[models.Artist(name=pandora_track.company_name)],
-                                         album=models.Album(name=pandora_track.company_name,
-                                                            uri=pandora_track.click_through_url)
-                                         )
-
+                    track_kwargs['name'] = 'Advertisement'
+                    artist_kwargs['name'] = getattr(pandora_track, 'company_name', 'Advertisement')
+                    album_kwargs['name'] = getattr(pandora_track, 'company_name', 'Advertisement')
+                    album_kwargs['uri'] = pandora_track.click_through_url
                 else:
-                    track = models.Track(name=pandora_track.song_name,
-                                         uri=uri,
-                                         length=pandora_track.track_length * 1000,
-                                         bitrate=int(pandora_track.bitrate),
-                                         artists=[models.Artist(name=pandora_track.artist_name)],
-                                         album=models.Album(name=pandora_track.album_name,
-                                                            uri=pandora_track.album_detail_url)
-                                         )
+                    track_kwargs['name'] = pandora_track.song_name
+                    track_kwargs['length'] = pandora_track.track_length * 1000
+                    track_kwargs['bitrate'] = int(pandora_track.bitrate)
+                    artist_kwargs['name'] = pandora_track.artist_name
+                    album_kwargs['name'] = pandora_track.album_name
+                    album_kwargs['uri'] = pandora_track.album_detail_url
         else:
-            raise ValueError('Unexpected URI type: {}'.format(uri))
+            raise ValueError("Unexpected type to perform track lookup: {}".format(pandora_uri.uri_type))
 
-        # TODO: Album.images has been deprecated in Mopidy 1.2. Remove this code when all frontends have been
-        #       updated to make use of the newer LibraryController.get_images()
-        images = self.get_images([uri])[uri]
-        if len(images) > 0:
-            album = track.album.replace(images=[images[0].uri])
-            track = track.replace(album=album)
-        return [track]
+        track_kwargs['artists'] = [models.Artist(**artist_kwargs)]
+        track_kwargs['album'] = models.Album(**album_kwargs)
+        return [models.Track(**track_kwargs)]
 
     def get_images(self, uris):
         result = {}
--- tests/test_library.py
+++ tests/test_library.py
@@ -20,7 +20,6 @@ from tests.conftest import get_station_list_mock
 
 
 def test_get_images_for_ad_without_images(config, ad_item_mock):
-
     backend = conftest.get_backend(config)
 
     ad_uri = PandoraUri.factory('pandora:ad:' + conftest.MOCK_TRACK_AD_TOKEN)
@@ -31,7 +30,6 @@ def test_get_images_for_ad_without_images(config, ad_item_mock):
 
 
 def test_get_images_for_ad_with_images(config, ad_item_mock):
-
     backend = conftest.get_backend(config)
 
     ad_uri = PandoraUri.factory('pandora:ad:' + conftest.MOCK_TRACK_AD_TOKEN)
@@ -42,7 +40,6 @@ def test_get_images_for_ad_with_images(config, ad_item_mock):
 
 
 def test_get_images_for_unknown_uri_returns_empty_list(config, caplog):
-
     backend = conftest.get_backend(config)
 
     track_uri = PandoraUri.factory('pandora:track:dummy_id:dummy_token')
@@ -52,7 +49,6 @@ def test_get_images_for_unknown_uri_returns_empty_list(config, caplog):
 
 
 def test_get_images_for_track_without_images(config, playlist_item_mock):
-
     backend = conftest.get_backend(config)
 
     track_uri = PandoraUri.factory('pandora:track:dummy_id:dummy_token')
@@ -63,7 +59,6 @@ def test_get_images_for_track_without_images(config, playlist_item_mock):
 
 
 def test_get_images_for_track_with_images(config, playlist_item_mock):
-
     backend = conftest.get_backend(config)
 
     track_uri = PandoraUri.factory('pandora:track:dummy_id:dummy_token')
@@ -73,11 +68,32 @@ def test_get_images_for_track_with_images(config, playlist_item_mock):
     assert results[track_uri.uri][0].uri == playlist_item_mock.album_art_url
 
 
-def test_lookup_of_invalid_uri(config, caplog):
+def test_lookup_of_invalid_uri(config):
     with pytest.raises(NotImplementedError):
         backend = conftest.get_backend(config)
 
         backend.library.lookup('pandora:invalid')
+
+
+def test_lookup_of_invalid_uri_type(config, caplog):
+    with pytest.raises(ValueError):
+        backend = conftest.get_backend(config)
+
+        backend.library.lookup('pandora:station:dummy_id:dummy_token')
+        assert 'Unexpected type to perform track lookup: station' in caplog.text()
+
+
+def test_lookup_of_ad_uri(config, ad_item_mock):
+    backend = conftest.get_backend(config)
+
+    track_uri = PlaylistItemUri._from_track(ad_item_mock)
+    backend.library._pandora_track_cache[track_uri.uri] = ad_item_mock
+
+    results = backend.library.lookup(track_uri.uri)
+    assert len(results) == 1
+
+    track = results[0]
+    assert track.uri == track_uri.uri
 
 
 def test_lookup_of_track_uri(config, playlist_item_mock):
