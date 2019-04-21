d7416687ca9a30b14630a3b124eda56780f31863
jcass77 <john.cass77@gmail.com>
2019-04-21 16:03:59 +0200

    Ignore flake8 warning.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 try:
     import queue
 except ImportError:
-    import Queue as queue
+    import Queue as queue  # noqa: N813
 
 import json
 
